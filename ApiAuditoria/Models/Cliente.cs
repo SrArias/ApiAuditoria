85a052accc64ecc62c8ca62a2cc5d8b7e0edee80
SrArias <SrArias>
2024-05-08 13:29:48 -0500

    subiendo

---
+++ ApiAuditoria/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiAuditoria.Models
+{
+    public class Cliente
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+        public string edad { get; set; }
+        public string correo { get; set; }
+
+    }
+}
